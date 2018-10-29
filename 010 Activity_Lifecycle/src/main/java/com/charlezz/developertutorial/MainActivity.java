b17eb99e98b11adc6ab383eec84b4a238ef4add4
Soohwan <soohwan.ok@navercorp.com>
2018-10-29 14:25:58 +0900

    액티비티 생명주기 설명 주석 추가

--- 010 Activity_Lifecycle/src/main/java/com/charlezz/developertutorial/MainActivity.java
+++ 010 Activity_Lifecycle/src/main/java/com/charlezz/developertutorial/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 /**
+ * @author Charlezz
  * @see <a href="https://www.charlezz.com/wordpress/wp-content/uploads/2018/10/173773234C63B8DA99.png">액티비티 생명주기</a>
+ * @see <a href="https://www.charlezz.com/?p=834">액티비티 생명주기</a>
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -15,41 +17,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e(TAG,"onCreate");
+        // 액티비티가 생성되는 중
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.e(TAG,"onRestart");
+        //액티비티가 중단되었다가 다시 시작
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.e(TAG,"onStart");
+        //액티비티가 화면에 나타나기 시작
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.e(TAG,"onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e(TAG,"onStop");
+        //액티비티가 화면에 나타나고 상호작용이 가능해짐
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.e(TAG,"onPause");
+        //다른 액티비티가 시작되려함, 이 액티비티는 중단되려하고 백그라운드로 들어갑니다.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG,"onStop");
+        //액티비티가 더 이상 화면에 나타나지 않음,중단된 상태
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.e(TAG,"onDestroy");
+        //액티비티가 종료되려고 합니다.
     }
 }
